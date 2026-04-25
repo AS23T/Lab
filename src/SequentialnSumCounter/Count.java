870d0e80ab13e3f1ddfc16e4f71e8db72eba3fb8
AStral <110380603+AS23T@users.noreply.github.com>
2026-04-25 01:45:45 -0400

    Clean up Count.java by removing license comments
    Removed unnecessary license comments and cleaned up the Count.java file for better readability.

--- src/SequentialnSumCounter/Count.java
+++ src/SequentialnSumCounter/Count.java
@@ -1,15 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package SequentialnSumCounter;
 
 public class Count {
-    //Atributos
+    
     private int QuantityPairs;
     private int DoubleJumps;
     
-    //Constructor
+    
     public Count() {
         this.QuantityPairs = 0;
         this.DoubleJumps = 0;
